f928bdedf44523e322b5ce005f95dbbc06eb1e91
larissa <larissafurtado12@live.com>
2020-11-01 18:17:11 -0300

    adding get and put from firebase using axios

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import actions from './actions'
+
 import stocks from './modules/stocks'
 import portfolio from './modules/portfolio'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    modules: {stocks, portfolio}
+    actions,
+    modules: {
+        stocks, 
+        portfolio}
 })
